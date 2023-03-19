6364f845710c8b4ab019c0aa6af63b040109fd32
saad <saddaddina@gmail.com>
2023-03-19 08:24:39 +0400

    first commit

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles'; 
+
+
+
+ const theme = createTheme({
+    typography: {
+      allVariants: {
+        fontFamily: "'Open Sans', sans-serif",
+       
+      },
+    },
+  });
+
+export default theme
